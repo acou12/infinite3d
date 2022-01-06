ac1fb6e0f77018c183d495b6da13d66aeb4541f6
acou12 <51276518+acou12@users.noreply.github.com>
2022-01-06 14:53:09 -0700

    Getting somewhere

--- src/main.ts
+++ src/main.ts
@@ -2,31 +2,177 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "lil-gui";
-import { CylinderGeometry, Object3D } from "three";
+import { CylinderGeometry, MeshStandardMaterial } from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import * as CANNON from "cannon";
+import boardVertexShader from "./shaders/board/vertex.glsl?raw";
+import boardFragmentShader from "./shaders/board/fragment.glsl?raw";
+
+const physicsWorld = new CANNON.World();
+
+physicsWorld.gravity.set(0, -9.82, 0);
+
+const plane = new CANNON.Body({
+  mass: 0,
+  position: new CANNON.Vec3(0, 0, 0),
+  shape: new CANNON.Plane(),
+});
+
+const bowlingBallMesh = new THREE.Mesh(
+  new THREE.SphereGeometry(1, 32, 32),
+  new THREE.MeshStandardMaterial({ color: "#222" })
+);
+
+const bowlingBallPhysics = new CANNON.Body({
+  mass: 1500,
+  position: new CANNON.Vec3(3.5, 20, -100),
+  shape: new CANNON.Sphere(1),
+});
+
+bowlingBallPhysics.velocity.set(0, 0, 30);
+
+// bowlingBallPhysics.velocity.set(-30, 0, 0);
+
+physicsWorld.addBody(bowlingBallPhysics);
+
+plane.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+
+physicsWorld.addBody(plane);
 
 const modelLoader = new GLTFLoader();
 
-let set: THREE.Group;
+type Color = "WHITE" | "BLACK";
 
-modelLoader.load("/chess.gltf", (gltf) => {
-  const chess = gltf.scene;
-  set = chess;
-  chess.scale.set(0.014, 0.014, 0.014);
-  chess.position.set(3.36, -1.81, 3.36);
-  scene.add(chess);
-  gui.add(chess.position, "x", -10, 10);
-  gui.add(chess.position, "y", -10, 10);
-  gui.add(chess.position, "z", -10, 10);
-  gui.add(chess.scale, "x", 0, 0.02).onChange((it: number) => {
-    chess.scale.y = it;
-    chess.scale.z = it;
+interface PieceType {
+  modelFileName: string;
+  model?: THREE.Object3D;
+}
+
+const PAWN: PieceType = {
+  modelFileName: "pawn.gltf",
+};
+
+const BISHOP: PieceType = {
+  modelFileName: "bishop.gltf",
+};
+
+const KNIGHT: PieceType = {
+  modelFileName: "knight.gltf",
+};
+
+const ROOK: PieceType = {
+  modelFileName: "rook.gltf",
+};
+
+const KING: PieceType = {
+  modelFileName: "king.gltf",
+};
+
+const QUEEN: PieceType = {
+  modelFileName: "queen.gltf",
+};
+
+const pieceTypes = [PAWN, BISHOP, KNIGHT, ROOK, KING, QUEEN];
+
+interface Piece {
+  physicsBody: CANNON.Body;
+  threeObject: THREE.Mesh<any, MeshStandardMaterial>;
+  type: PieceType;
+  color: Color;
+  position: THREE.Vector2;
+}
+
+const pieces: Piece[] = [];
+let selectedPiece: Piece | null = null;
+
+let numLoaded = 0;
+
+pieceTypes.forEach((type) => {
+  const { modelFileName } = type;
+  modelLoader.load(`/${modelFileName}`, (gltf) => {
+    const model = gltf.scene;
+    type.model = model;
+    model.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        child.material = new THREE.MeshNormalMaterial();
+      }
+    });
+    numLoaded++;
+    if (numLoaded >= pieceTypes.length) {
+      addPieces();
+    }
   });
 });
 
-// Shaders
-import boardVertexShader from "./shaders/board/vertex.glsl?raw";
-import boardFragmentShader from "./shaders/board/fragment.glsl?raw";
+function addPiece(
+  pieceType: PieceType,
+  x: number,
+  y: number,
+  color: Color
+): void {
+  const { model } = pieceType;
+
+  const box = new THREE.Box3().setFromObject(model!);
+  const threeObject = model!.clone();
+
+  threeObject.traverse((child) => {
+    if (child instanceof THREE.Mesh) {
+      child.material = new THREE.MeshStandardMaterial(
+        color === "WHITE" ? { color: "#fff" } : { color: "#222" }
+      );
+    }
+  });
+
+  threeObject.position.set(x, -box.min.y, y);
+  scene.add(threeObject);
+
+  const physicsBody = new CANNON.Body({
+    mass: 5,
+    position: new CANNON.Vec3(x, 3, y),
+    shape: new CANNON.Box(
+      new CANNON.Vec3(
+        (box.max.x - box.min.x) / 2,
+        (box.max.y - box.min.y) / 2,
+        (box.max.z - box.min.z) / 2
+      )
+    ),
+  });
+
+  // physicsBody.angularVelocity.set(10, 10);
+  physicsWorld.addBody(physicsBody);
+
+  pieces.push({
+    physicsBody,
+    threeObject: threeObject as THREE.Mesh<any, MeshStandardMaterial>,
+    type: pieceType,
+    color,
+    position: new THREE.Vector2(x, y),
+  });
+}
+
+function addPieces() {
+  for (let i = 0; i < 8; i++) {
+    addPiece(PAWN, i, 1, "WHITE");
+    addPiece(PAWN, i, 6, "BLACK");
+  }
+
+  addPiece(ROOK, 0, 0, "WHITE");
+  addPiece(ROOK, 7, 0, "WHITE");
+  addPiece(ROOK, 0, 7, "BLACK");
+  addPiece(ROOK, 7, 7, "BLACK");
+  addPiece(KNIGHT, 1, 0, "WHITE");
+  addPiece(KNIGHT, 6, 0, "WHITE");
+  addPiece(KNIGHT, 1, 7, "BLACK");
+  addPiece(KNIGHT, 6, 7, "BLACK");
+  addPiece(BISHOP, 2, 0, "WHITE");
+  addPiece(BISHOP, 5, 0, "WHITE");
+  addPiece(BISHOP, 2, 7, "BLACK");
+  addPiece(BISHOP, 5, 7, "BLACK");
+  addPiece(QUEEN, 3, 0, "WHITE");
+  addPiece(QUEEN, 3, 7, "BLACK");
+  addPiece(KING, 4, 0, "WHITE");
+  addPiece(KING, 4, 7, "BLACK");
+}
 
 /**
  * Base
@@ -37,6 +183,8 @@ const gui = new dat.GUI();
 const canvas = document.querySelector("canvas")!;
 
 const scene = new THREE.Scene();
+
+scene.add(bowlingBallMesh);
 
 // TODO: Credit
 // "Chess Board" (https://skfb.ly/6BDGq) by Anthony Yanez is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
@@ -53,9 +201,9 @@ window.addEventListener("mousemove", (event: MouseEvent) => {
 window.addEventListener("click", () => {
   if (hoveredMeshes.length > 0) {
     const mesh = hoveredMeshes[0].object;
-    const meshes = pieces.map((piece) => piece.mesh);
-    const index = meshes.indexOf(mesh);
-    selected = index;
+    const piece = pieces.filter((p) => p.threeObject === mesh)[0];
+    piece.physicsBody.velocity.set(0, 10, 0);
+    piece.physicsBody.angularVelocity.set(1, 2, 3);
   }
 });
 
@@ -64,72 +212,19 @@ window.addEventListener("click", () => {
  */
 const geometry = new THREE.CylinderGeometry(0.4, 0.4, 2, 32, 32);
 
-const WHITE_COLOR = "#fff";
-const BLACK_COLOR = "#222";
-
 type PieceMesh = THREE.Mesh<CylinderGeometry, THREE.MeshStandardMaterial>;
 
-interface Piece {
-  mesh: PieceMesh;
-  position: THREE.Vector2;
-  color: "WHITE" | "BLACK";
-}
-
-const pieces: Piece[] = [];
-
-// for (let i = 0; i < 8 * 2; i++) {
-//   let row = Math.floor(i / 8);
-//   let col = i % 8;
-//   const piece = new THREE.Mesh(
-//     geometry,
-//     new THREE.MeshStandardMaterial({
-//       color: WHITE_COLOR,
-//     })
-//   );
-
-//   piece.position.set(row, 0, col);
-//   pieces.push({
-//     mesh: piece,
-//     position: new THREE.Vector2(row, col),
-//     color: "WHITE",
-//   });
-//   scene.add(piece);
+// interface Piece {
+//   mesh: PieceMesh;
+//   position: THREE.Vector2;
+//   color: "WHITE" | "BLACK";
 // }
 
-// for (let i = 0; i < 8 * 2; i++) {
-//   let row = Math.floor(i / 8);
-//   let col = i % 8;
-//   const piece = new THREE.Mesh(
-//     geometry,
-//     new THREE.MeshStandardMaterial({
-//       color: BLACK_COLOR,
-//     })
-//   );
-
-//   piece.position.set(7 - row, 0, col);
-//   pieces.push({
-//     mesh: piece,
-//     position: new THREE.Vector2(7 - row, col),
-//     color: "BLACK",
-//   });
-//   scene.add(piece);
-// }
+// const pieces: Piece[] = [];
 
 // Board
 const boardGeometry = new THREE.PlaneGeometry(250, 250, 250, 250);
 boardGeometry.rotateX(-Math.PI / 2);
-// const whiteBoardPiece = new THREE.Mesh(
-//   boardGeometry,
-//   new THREE.MeshStandardMaterial({
-//     color: "#f0d9b5",
-//   })
-// );
-// const blackBoardPiece = new THREE.Mesh(
-//   boardGeometry,
-//   new THREE.MeshStandardMaterial({
-//     color: "#b58863",
-//   })
-// );
 
 const board = new THREE.Mesh(
   boardGeometry,
@@ -138,8 +233,6 @@ const board = new THREE.Mesh(
     fragmentShader: boardFragmentShader,
   })
 );
-
-board.position.y -= 1;
 
 scene.add(board);
 
@@ -165,7 +258,7 @@ window.addEventListener("resize", () => {
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
 
-  // camera.aspect = sizes.width / sizes.height;
+  camera.aspect = sizes.width / sizes.height;
   camera.updateProjectionMatrix();
 
   renderer.setSize(sizes.width, sizes.height);
@@ -196,7 +289,7 @@ scene.add(camera);
 
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-controls.enablePan = false;
+// controls.enablePan = false;
 controls.target.set(3.5, 0, 3.5);
 
 /**
@@ -218,21 +311,25 @@ gui.addColor(guiItems, "clearColor").onChange(renderer.setClearColor);
 /**
  * Animate
  */
-// const clock = new THREE.Clock();
+const clock = new THREE.Clock();
 
 const raycaster = new THREE.Raycaster();
 
 let hoveredMeshes: THREE.Intersection<PieceMesh>[] = [];
-let selected: number = -1;
+
+var fixedTimeStep = 1.0 / 60.0;
+var maxSubSteps = 3;
 
 const tick = () => {
   // const elapsedTime = clock.getElapsedTime();
-  const meshes = pieces.map((piece) => piece.mesh);
+  const meshes = pieces.map((piece) => piece.threeObject);
 
   pieces.forEach((piece) => {
-    piece.mesh.material.color.set(
-      piece.color === "WHITE" ? WHITE_COLOR : BLACK_COLOR
-    );
+    piece.threeObject.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        child.material.color.set(piece.color === "WHITE" ? "#fff" : "#222");
+      }
+    });
   });
 
   raycaster.setFromCamera(mousePosition, camera);
@@ -243,10 +340,26 @@ const tick = () => {
     mesh.material.color.set("#f00");
   }
 
-  if (selected > -1) meshes[selected].material.color.set("#0f0");
+  // if (selected > -1) meshes[selected].material.color.set("#0f0");
 
   controls.update();
 
+  pieces.forEach((piece) => {
+    piece.threeObject.position.x = piece.physicsBody.position.x;
+    piece.threeObject.position.y = piece.physicsBody.position.y;
+    piece.threeObject.position.z = piece.physicsBody.position.z;
+
+    piece.threeObject.quaternion.x = piece.physicsBody.quaternion.x;
+    piece.threeObject.quaternion.y = piece.physicsBody.quaternion.y;
+    piece.threeObject.quaternion.z = piece.physicsBody.quaternion.z;
+    piece.threeObject.quaternion.w = piece.physicsBody.quaternion.w;
+  });
+
+  bowlingBallMesh.position.x = bowlingBallPhysics.position.x;
+  bowlingBallMesh.position.y = bowlingBallPhysics.position.y;
+  bowlingBallMesh.position.z = bowlingBallPhysics.position.z;
+
+  physicsWorld.step(fixedTimeStep, clock.getDelta(), maxSubSteps);
   renderer.render(scene, camera);
 
   window.requestAnimationFrame(tick);
