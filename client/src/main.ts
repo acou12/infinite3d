20d07f820c356ec77a8e9275282a9f6072e3b947
acou12 <51276518+acou12@users.noreply.github.com>
2023-01-16 10:36:23 -0500

    black hole

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,209 +1,20 @@
 import * as THREE from "three";
-import * as CANNON from "cannon";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-
 import "./style.css";
+import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader";
+
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass";
+
 import boardVertexShader from "./shaders/board/vertex.glsl?raw";
 import boardFragmentShader from "./shaders/board/fragment.glsl?raw";
-import { AnimationDispatcher } from "./animation";
-import { ServerInterface } from "./server";
-import { MeshStandardMaterial, Vector3 } from "three";
-
-const serverInterface = new ServerInterface();
-serverInterface.onmove = (from, to) => {
-  const matchingPieces = pieces.filter(
-    (piece) => piece.position.x === from.x && piece.position.y === from.y
-  );
-  console.log(from, to);
-  if (!(matchingPieces.length > 0)) throw new Error("uh oh, desync!!");
-  const newTurn = turn === "WHITE" ? "BLACK" : "WHITE";
-  turn = null;
-  movePiece(matchingPieces[0], to, () => {
-    turn = newTurn;
-  });
-};
-
-const physicsWorld = new CANNON.World();
-
-physicsWorld.gravity.set(0, -15, 0);
-
-const floorPhysics = new CANNON.Body({
-  mass: 0,
-  position: new CANNON.Vec3(0, 0, 0),
-  shape: new CANNON.Plane(),
-});
-
-floorPhysics.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
-
-// const bowlingBallMesh = new THREE.Mesh(
-//   new THREE.SphereGeometry(4, 32, 32),
-//   new THREE.MeshStandardMaterial({ color: "#222" })
-// );
-
-// const bowlingBallPhysics = new CANNON.Body({
-//   mass: 15000,
-//   position: new CANNON.Vec3(1000, 2, 0.5),
-//   shape: new CANNON.Sphere(bowlingBallMesh.geometry.parameters.radius),
-// });
-
-// plane.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
-
-physicsWorld.addBody(floorPhysics);
-
-const modelLoader = new GLTFLoader();
-
-type Color = "WHITE" | "BLACK";
-
-interface PieceType {
-  modelFileName: string;
-  model?: THREE.Object3D;
-}
-
-const PAWN: PieceType = { modelFileName: "pawn.gltf" };
-const BISHOP: PieceType = { modelFileName: "bishop.gltf" };
-const KNIGHT: PieceType = { modelFileName: "knight.gltf" };
-const ROOK: PieceType = { modelFileName: "rook.gltf" };
-const KING: PieceType = { modelFileName: "king.gltf" };
-const QUEEN: PieceType = { modelFileName: "queen.gltf" };
-
-const pieceTypes = [PAWN, BISHOP, KNIGHT, ROOK, KING, QUEEN];
-
-interface Piece {
-  physicsBody: CANNON.Body;
-  threeObject: THREE.Mesh<any, THREE.MeshStandardMaterial>;
-  type: PieceType;
-  color: Color;
-  position: THREE.Vector2;
-}
-
-const pieces: Piece[] = [];
-
-let numLoaded = 0;
-
-pieceTypes.forEach((type) => {
-  const { modelFileName } = type;
-  modelLoader.load(`/models/${modelFileName}`, (gltf) => {
-    const model = gltf.scene;
-    type.model = model;
-    model.traverse((child) => {
-      if (child instanceof THREE.Mesh) {
-        child.material = new THREE.MeshNormalMaterial();
-      }
-    });
-    numLoaded++;
-    if (numLoaded >= pieceTypes.length) {
-      afterTexturesLoaded();
-    }
-  });
-});
-
-function addPiece(
-  pieceType: PieceType,
-  x: number,
-  y: number,
-  color: Color
-): void {
-  const { model } = pieceType;
-
-  const box = new THREE.Box3().setFromObject(model!);
-  const threeObject = model!.clone();
-
-  threeObject.traverse((child) => {
-    if (child instanceof THREE.Mesh) {
-      child.material = new THREE.MeshStandardMaterial(
-        color === "WHITE" ? { color: "#fff" } : { color: "#222" }
-      );
-    }
-  });
-
-  threeObject.position.set(x, -box.min.y, y);
-  scene.add(threeObject);
-
-  const physicsBody = new CANNON.Body({
-    mass: 5,
-    position: new CANNON.Vec3(x, 3, y),
-    shape: new CANNON.Box(
-      new CANNON.Vec3(
-        (box.max.x - box.min.x) / 2,
-        (box.max.y - box.min.y) / 2,
-        (box.max.z - box.min.z) / 2
-      )
-    ),
-  });
-
-  physicsWorld.addBody(physicsBody);
-
-  pieces.push({
-    physicsBody,
-    threeObject: threeObject as THREE.Mesh<any, THREE.MeshStandardMaterial>,
-    type: pieceType,
-    color,
-    position: new THREE.Vector2(x, y),
-  });
-}
-
-function addPieces() {
-  for (let i = 0; i < 8; i++) {
-    addPiece(PAWN, i, 1, "WHITE");
-    addPiece(PAWN, i, 6, "BLACK");
-  }
-
-  addPiece(ROOK, 0, 0, "WHITE");
-  addPiece(ROOK, 7, 0, "WHITE");
-  addPiece(KNIGHT, 1, 0, "WHITE");
-  addPiece(KNIGHT, 6, 0, "WHITE");
-  addPiece(BISHOP, 2, 0, "WHITE");
-  addPiece(BISHOP, 5, 0, "WHITE");
-  addPiece(QUEEN, 4, 0, "WHITE");
-  addPiece(KING, 3, 0, "WHITE");
-  addPiece(ROOK, 0, 7, "BLACK");
-  addPiece(ROOK, 7, 7, "BLACK");
-  addPiece(KNIGHT, 1, 7, "BLACK");
-  addPiece(KNIGHT, 6, 7, "BLACK");
-  addPiece(BISHOP, 2, 7, "BLACK");
-  addPiece(BISHOP, 5, 7, "BLACK");
-  addPiece(QUEEN, 4, 7, "BLACK");
-  addPiece(KING, 3, 7, "BLACK");
-}
 
 const canvas = document.querySelector("canvas")!;
 
 const scene = new THREE.Scene();
 
-// "Chess Board" (https://skfb.ly/6BDGq) by Anthony Yanez is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
-
+const previousMousePosition: THREE.Vector2 = new THREE.Vector2();
 const mousePosition: THREE.Vector2 = new THREE.Vector2();
-
-window.addEventListener("mousemove", (event: MouseEvent) => {
-  mousePosition.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mousePosition.y = -(event.clientY / window.innerHeight) * 2 + 1;
-});
-window.addEventListener("touchmove", (event: TouchEvent) => {
-  mousePosition.x = (event.touches[0].clientX / window.innerWidth) * 2 - 1;
-  mousePosition.y = -(event.touches[0].clientY / window.innerHeight) * 2 + 1;
-});
-
-function handleClick(_event: MouseEvent | TouchEvent) {
-  if (hoveredCard !== undefined) selectedCard = hoveredCard;
-  else if (selectedCard !== undefined && hoveredPoint !== undefined) {
-    hand.remove(selectedCard);
-    cards.splice(cards.indexOf(selectedCard), 1);
-    selectedCard = undefined;
-    pieces.forEach((piece) => {
-      const delta = piece.threeObject.position.clone().sub(hoveredPoint!);
-      const launch = 30 / Math.pow(delta.length(), 2);
-      delta.normalize();
-      delta.multiplyScalar(launch);
-      piece.physicsBody.velocity.set(delta.x, delta.y, delta.z);
-    });
-  }
-}
-
-window.addEventListener("click", handleClick);
-window.addEventListener("touchstart", handleClick);
-
-// type PieceMesh = THREE.Mesh<THREE.CylinderGeometry, THREE.MeshStandardMaterial>;
 
 const boardGeometry = new THREE.PlaneGeometry(5000, 5000, 10, 10);
 
@@ -225,12 +36,6 @@ board.rotateX(-Math.PI / 2);
 board.position.x += 3;
 board.position.z += 3;
 
-scene.add(new THREE.AmbientLight("white", 0.5));
-const pointLight = new THREE.PointLight("white", 0.5);
-
-pointLight.position.set(3.5, 3, 3.5);
-scene.add(pointLight);
-
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
@@ -240,44 +45,24 @@ window.addEventListener("resize", () => {
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
 
-  camera.aspect = sizes.width / sizes.height;
+  // camera.aspect = sizes.width / sizes.height;
   camera.updateProjectionMatrix();
 
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
+const camera = new THREE.OrthographicCamera(
+  -sizes.width / 2 / 30,
+  sizes.width / 2 / 30,
+  sizes.height / 2 / 30,
+  -sizes.height / 2 / 30,
+  1,
   1000
 );
-camera.position.set(10, 5, 10);
-camera.lookAt(3.5, 0, 3.5);
+camera.position.set(0, 10, 0);
+camera.lookAt(0, 0, 0);
 scene.add(camera);
-
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
-controls.target.set(3.5, 0, 3.5);
-controls.autoRotate = false;
-controls.autoRotateSpeed = 0.1;
-let lastCameraPosition = new THREE.Vector3();
-controls.addEventListener("change", () => {
-  const cameraPosition = camera.position.clone();
-  if (
-    cameraPosition
-      .clone()
-      .sub(lastCameraPosition)
-      .clone()
-      .cross(lastCameraPosition.clone().sub(controls.target)).y > 0
-  ) {
-    controls.autoRotateSpeed = 0.1;
-  } else {
-    controls.autoRotateSpeed = -0.1;
-  }
-  lastCameraPosition = cameraPosition;
-});
 
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
@@ -286,149 +71,124 @@ renderer.setClearColor("#ff8000");
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+const composer = new EffectComposer(renderer);
+
+[
+  new RenderPass(scene, camera),
+  new ShaderPass({
+    uniforms: {
+      tDiffuse: { value: null },
+      opacity: { value: 1.0 },
+      ratio: { value: sizes.height / sizes.width },
+    },
+
+    vertexShader: /* glsl */ `
+		varying vec2 vUv;
+		void main() {
+			vUv = uv;
+			gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+		}`,
+
+    fragmentShader: /* glsl */ `
+		uniform float opacity;
+		uniform sampler2D tDiffuse;
+		uniform float ratio;
+		varying vec2 vUv;
+		void main() {
+      float angle = atan(vUv.y - 0.5, vUv.x - 0.5);
+      float dist = sqrt((vUv.x - 0.5) * (vUv.x - 0.5) + (vUv.y - 0.5) * (vUv.y - 0.5));
+			gl_FragColor = texture2D( 
+        tDiffuse, 
+        vec2(
+          dist * cos(angle + 0.3 / min(0.5, dist) - 3.0 / 5.0) + 0.5,
+          dist * sin(angle + 0.3 / min(0.5, dist) - 3.0 / 5.0) + 0.5
+        )
+      );
+			gl_FragColor.a *= opacity;
+    }`,
+  }),
+].map((pass) => composer.addPass(pass));
+
 const clock = new THREE.Clock();
-
-const animationDispatcher = new AnimationDispatcher();
-
-function afterTexturesLoaded() {
-  addPieces();
-}
-
-let turn: Color | null = "WHITE";
-
-function movePiece(piece: Piece, to: THREE.Vector2, doneCallback?: () => void) {
-  const pieceAt = pieces.filter((p) => p.position.equals(to))[0];
-  if (pieceAt) {
-    animationDispatcher.slide({
-      position: pieceAt.threeObject.position,
-      to: pieceAt.threeObject.position.clone().setY(20),
-      duration: 1.5,
-      type: "SLIDE",
-      doneCallback: () => {
-        scene.remove(pieceAt.threeObject);
-        pieceAt.threeObject.material?.dispose();
-        pieceAt.threeObject.geometry?.dispose();
-      },
-    });
-  }
-  piece.position = to;
-  animationDispatcher.slide({
-    position: piece.threeObject.position,
-    to: new THREE.Vector3(to.x, 0, to.y).setY(piece.threeObject.position.y),
-    duration: 1.5,
-    type: piece.type === KNIGHT ? "JUMP" : "SLIDE",
-    doneCallback,
-  });
-}
-
-const keyPressed: Record<string, boolean> = {};
-
-const cards: THREE.Mesh[] = [];
-
-const hand = new THREE.Group();
 
 const textureLoader = new THREE.TextureLoader();
 
-textureLoader.loadAsync("/grenade.jpg").then((texture) => {
-  const createCard = () => {
-    const card = new THREE.Mesh(
-      new THREE.BoxGeometry(1, 2, 0.1),
-      new THREE.MeshStandardMaterial({
-        map: texture,
-      })
-    );
-    // card.position.set(0, 0, 0);
-    hand.add(card);
-    cards.push(card);
-    return card;
-  };
-
-  for (let i = 0; i < 10; i++) createCard();
-
-  let i = -cards.length / 2 + 0.5;
-  for (const card of cards) {
-    // card.rotateY(-(i * Math.PI) / 6);
-    // card.rotateX((-i * Math.PI) / 7);
-    card.position.x = i * 1.1;
-    card.position.y -= 2.5;
-    // card.position.z += (i * i) / 10;
-    // card.lookAt(camera.position);
-    i++;
-  }
-});
-
-scene.add(hand);
-
-document.addEventListener("keydown", (event) => {
-  keyPressed[event.key.toLowerCase()] = true;
-});
-
-document.addEventListener("keyup", (event) => {
-  keyPressed[event.key.toLowerCase()] = false;
-});
-
 const raycaster = new THREE.Raycaster();
-
-let selectedCard: THREE.Mesh | undefined;
-let hoveredCard: THREE.Mesh | undefined;
-
-let hoveredPoint: THREE.Vector3 | undefined;
 
 function tick() {
   const delta = clock.getDelta();
 
-  controls.update();
-  animationDispatcher.update(delta);
-
   raycaster.setFromCamera(mousePosition, camera);
 
-  cards.forEach(
-    (card) =>
-      ((card.material as MeshStandardMaterial).color =
-        card === selectedCard
-          ? new THREE.Color("blue")
-          : new THREE.Color("white"))
-  );
-
-  hoveredCard = undefined;
-
-  const cardIntersections = raycaster.intersectObjects(cards);
-  cardIntersections.forEach((intersection) => {
-    (
-      (intersection.object as THREE.Mesh).material as MeshStandardMaterial
-    ).color = new THREE.Color("red");
-    hoveredCard = intersection.object as THREE.Mesh;
-  });
-
-  hoveredPoint = undefined;
-
-  const boardIntersections = raycaster.intersectObject(board);
-  boardIntersections.forEach((intersection) => {
-    hoveredPoint = intersection.point;
-  });
-
-  const v = new Vector3(0, 0, -5);
-  v.applyQuaternion(camera.quaternion);
-  hand.setRotationFromQuaternion(camera.quaternion);
-  const newPos = camera.position.clone().add(v);
-  hand.position.set(newPos.x, newPos.y, newPos.z);
-
-  pieces.forEach((piece) => {
-    piece.threeObject.position.x = piece.physicsBody.position.x;
-    piece.threeObject.position.y = piece.physicsBody.position.y;
-    piece.threeObject.position.z = piece.physicsBody.position.z;
-
-    piece.threeObject.quaternion.x = piece.physicsBody.quaternion.x;
-    piece.threeObject.quaternion.y = piece.physicsBody.quaternion.y;
-    piece.threeObject.quaternion.z = piece.physicsBody.quaternion.z;
-    piece.threeObject.quaternion.w = piece.physicsBody.quaternion.w;
-  });
-
-  physicsWorld.step(1 / 60, delta, 1);
-
-  renderer.render(scene, camera);
+  // renderer.render(scene, camera);
+  composer.render();
 
   window.requestAnimationFrame(tick);
 }
+
+let pressing: boolean = false;
+
+window.addEventListener("mousedown", (event: MouseEvent) => {
+  pressing = true;
+});
+
+window.addEventListener("mouseup", (event: MouseEvent) => {
+  pressing = false;
+});
+
+window.addEventListener("mousemove", (event: MouseEvent) => {
+  previousMousePosition.setX(mousePosition.x);
+  previousMousePosition.setY(mousePosition.y);
+  mousePosition.setX(event.clientX);
+  mousePosition.setY(event.clientY);
+
+  if (pressing) {
+    camera.position.add(
+      new THREE.Vector3(
+        -(mousePosition.x - previousMousePosition.x) / 30 / camera.zoom,
+        0,
+        -(mousePosition.y - previousMousePosition.y) / 30 / camera.zoom
+      )
+    );
+  }
+});
+
+window.addEventListener("wheel", (event: WheelEvent) => {
+  if (event.deltaY < 0) {
+    camera.zoom *= 1.1;
+  } else {
+    camera.zoom /= 1.1;
+  }
+  camera.updateProjectionMatrix();
+});
+
+const svgLoader = new SVGLoader();
+
+const king = new THREE.Group();
+
+let i = 0;
+
+svgLoader.load("/images/white-king.svg", (data) => {
+  data.paths.forEach((path) => {
+    SVGLoader.createShapes(path).forEach((shape) => {
+      const mesh = new THREE.Mesh(
+        new THREE.ShapeGeometry(shape),
+        new THREE.MeshBasicMaterial({
+          color: path.userData!.style.fill,
+          side: THREE.DoubleSide,
+          depthWrite: false,
+        })
+      );
+      king.add(mesh);
+    });
+  });
+  king.rotateX(Math.PI / 2);
+  king.scale.multiplyScalar(1 / 45);
+  for (let i = 0; i < 100; i++) {
+    const kingClone = king.clone();
+    kingClone.position.set((i % 10) - 0.5, 1, Math.floor(i / 10) - 0.5);
+    scene.add(kingClone);
+  }
+});
 
 tick();
